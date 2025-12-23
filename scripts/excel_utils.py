310ea1e5c608a9f7d0c02c37097e279415665926
sucaanalytics-max <sucaanalytics@gmail.com>
2025-12-23 11:44:50 +0530

    Update excel_utils.py

--- scripts/excel_utils.py
+++ scripts/excel_utils.py
@@ -3,14 +3,19 @@ from openpyxl import load_workbook
 
 EXCEL_PATH = "data/nse_bse_business_growth.xlsx"
 
-def append_unique(sheet_name: str, df_new: pd.DataFrame, date_col="date"):
+def append_unique(sheet_name: str, new_df: pd.DataFrame, date_col="Date"):
     book = load_workbook(EXCEL_PATH)
+
     if sheet_name in book.sheetnames:
-        df_old = pd.read_excel(EXCEL_PATH, sheet_name=sheet_name)
-        df = pd.concat([df_old, df_new], ignore_index=True)
-        df = df.drop_duplicates(subset=[date_col], keep="last")
+        old_df = pd.read_excel(EXCEL_PATH, sheet_name=sheet_name)
+
+        if date_col in old_df.columns:
+            old_dates = set(old_df[date_col].astype(str))
+            new_df = new_df[~new_df[date_col].astype(str).isin(old_dates)]
+
+        final_df = pd.concat([old_df, new_df], ignore_index=True)
     else:
-        df = df_new
+        final_df = new_df
 
     with pd.ExcelWriter(
         EXCEL_PATH,
@@ -19,4 +24,4 @@ def append_unique(sheet_name: str, df_new: pd.DataFrame, date_col="date"):
         if_sheet_exists="replace"
     ) as writer:
         writer.book = book
-        df.to_excel(writer, sheet_name=sheet_name, index=False)
+        final_df.to_excel(writer, sheet_name=sheet_name, index=False)
